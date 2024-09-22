9d072f11604fc428c3f8dec8b2af381e93232a7d
amorimdofront <pablo_pan2015@outlook.com>
2024-09-22 13:57:55 -0300

    primeiro commit

---
+++ src/mp.js
@@ -0,0 +1,11 @@
+document.getElementById("comprar1").addEventListener("click", function() {
+  window.open("pagamento1.html", "_blank"); // Substitua pela URL da página de destino
+});
+
+document.getElementById("comprar2").addEventListener("click", function() {
+  window.open("pagamento2.html", "_blank"); // Substitua pela URL da página de destino
+});
+
+document.getElementById("comprar3").addEventListener("click", function() {
+  window.open("pagamento3.html", "_blank"); // Substitua pela URL da página de destino
+});
